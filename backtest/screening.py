5f58df36a2d26964926cb5d0a59a5d9dc458d795
chris-ch <ch.alexandre@bluewin.ch>
2014-11-14 10:50:38 +0100

    fixed timeout too low

--- backtest/screening.py
+++ backtest/screening.py
@@ -21,30 +21,34 @@ def month_subtract(yyyymm, n):
     start_yyyymm = '%d%02d' % (start_yyyy, start_mm)
     return start_yyyymm
 
-def compute_volatility(security_code, performances, count_months, start_yyyymm, end_yyyymm):
+def compute_volatility(security_code, count_months, start_yyyymm, end_yyyymm):
+    performances = shelve.open(constants.CACHE_PERFS, 'r')
     security_performances = [performances[security_code][date]
             for date in performances[security_code].keys()
             if date[:6] <= end_yyyymm
             and date[:6] >= start_yyyymm
         ]
-    if len(security_performances) <= 0.8 * (count_months * 20): continue # not enough data
+    performances.close()
+    if len(security_performances) <= 0.8 * (count_months * 20): return None # not enough data
     
     return stdev(security_performances)
 
-def make_volatilities_statistics(universe, performances, count_months, start_yyyymm, end_yyyymm):
+def make_volatilities_statistics(universe, count_months, start_yyyymm, end_yyyymm):
     volatilities = dict()
     total = universe.size()
     from multiprocessing import Pool
     pool = Pool(processes=4) 
     results = dict()
     for index, security_code in enumerate(universe.securities()):
-        params = [security_code, performances, count_months, start_yyyymm, end_yyyymm]
+        params = [security_code, count_months, start_yyyymm, end_yyyymm]
         results[security_code] = pool.apply_async(compute_volatility, params)
         #logging.debug('volatility for security %s (%d/%d) = %.2f%%' % (security_code, index + 1, total, volatility * 100.0))
         #volatilities[security_code] = compute_volatility(security_code, performances, count_months, start_yyyymm, end_yyyymm)
     
     for security_code in results.keys():
-        volatilities[security_code] = results[security_code].get(timeout=60) 
+        result = results[security_code].get(timeout=30*60) # max 30 min
+        if result:
+            volatilities[security_code] = result
     
     return volatilities
 
@@ -83,8 +87,8 @@ class Screening(object):
         logging.info('considering volatility over [%s; %s]' % (start_yyyymm, end_yyyymm))
         volatilities = dict()
         
-        def stats_builder(universe=self.__universe, perfs=self.__performances, cm=count_months, sm=start_yyyymm, em=end_yyyymm):
-            return make_volatilities_statistics(universe, perfs, cm, sm, em)
+        def stats_builder(universe=self.__universe, cm=count_months, sm=start_yyyymm, em=end_yyyymm):
+            return make_volatilities_statistics(universe, cm, sm, em)
         
         cache = SimpleCache(constants.CACHE_SCREENING, stats_builder, key_builder=lambda a, b: str((a, b)))
         volatilities = cache.get(yyyymm, count_months)
